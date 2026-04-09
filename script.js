5bd0fe4d5d1c90513dcf53c0cfd7a98bb3db45c4
mushafirr <diwyankk@gmail.com>
2026-04-09 18:53:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-// simple click animation
-const links = document.querySelectorAll(".links a");
+const colors = [
+    "#000000",
+    "#1a1a00",
+    "#111100",
+    "#222200"
+];
 
-links.forEach(link => {
+let i = 0;
+
+setInterval(() => {
+    document.body.style.background = colors[i];
+    i = (i + 1) % colors.length;
+}, 3000);
+
+// click animation
+document.querySelectorAll(".links a").forEach(link => {
     link.addEventListener("click", () => {
         link.style.transform = "scale(0.95)";
         setTimeout(() => {
